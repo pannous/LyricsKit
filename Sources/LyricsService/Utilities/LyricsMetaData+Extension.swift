b4ed8d1c3c7b9a6a53a869a818a3623fcc5da89e
pannous <info@pannous.com>
2025-12-02 10:53:54 +0100

    feat: add KKBOX lyrics provider

---
+++ Sources/LyricsService/Utilities/LyricsMetaData+Extension.swift
@@ -0,0 +1,58 @@
+//
+//  LyricsMetaData+Extension.swift
+//
+//  This file is part of LyricsX - https://github.com/pannous/LyricsX
+//  Licensed under GPLv3.
+//
+
+import Foundation
+import LyricsCore
+
+extension Lyrics.MetaData.Key {
+    public static var request       = Lyrics.MetaData.Key("request")
+    public static var remoteURL     = Lyrics.MetaData.Key("remoteURL")
+    public static var artworkURL    = Lyrics.MetaData.Key("artworkURL")
+    public static var service       = Lyrics.MetaData.Key("service")
+    public static var serviceToken  = Lyrics.MetaData.Key("serviceToken")
+    static var quality              = Lyrics.MetaData.Key("quality")
+    
+    static var searchIndex          = Lyrics.MetaData.Key("searchIndex")
+}
+
+extension Lyrics.MetaData {
+    
+    public var request: LyricsSearchRequest? {
+        get { return data[.request] as? LyricsSearchRequest }
+        set { data[.request] = newValue }
+    }
+    
+    public var remoteURL: URL? {
+        get { return data[.remoteURL] as? URL }
+        set { data[.remoteURL] = newValue }
+    }
+    
+    public var artworkURL: URL? {
+        get { return data[.artworkURL] as? URL }
+        set { data[.artworkURL] = newValue }
+    }
+    
+    public var service: LyricsProviders.Service? {
+        get { return data[.service] as? LyricsProviders.Service }
+        set { data[.service] = newValue }
+    }
+    
+    public var serviceToken: String? {
+        get { return data[.serviceToken] as? String }
+        set { data[.serviceToken] = newValue }
+    }
+    
+    var quality: Double? {
+        get { return data[.quality] as? Double }
+        set { data[.quality] = newValue }
+    }
+    
+    var searchIndex: Int {
+        get { return data[.searchIndex] as? Int ?? 0 }
+        set { data[.searchIndex] = newValue }
+    }
+}
