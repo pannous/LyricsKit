cb6ae89633f0ffd5e04894dc3e96f9e1e8b648a7
pannous <info@pannous.com>
2026-04-20 08:56:21 +0200

    chore: add .gitignore and remove build artifacts from tracking

--- .build.x/arm64-apple-macosx/debug/system_zlib.build/anchor.c.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.3.sdk/usr/include/module.modulemap \
-  /Users/me/dev/apps/Lyrics-Translator/LyricsKit/.build/checkouts/GzipSwift/Sources/system-zlib/anchor.c
